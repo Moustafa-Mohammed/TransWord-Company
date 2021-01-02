42578501824a819b99ab4c58e968206d7699fd7f
Moustafa-Mohameed <thedevelopermostafa@gmail.com>
2021-01-02 11:19:26 +0200

    change the navbar style and add some functionality with JS

--- TransWord/js/main.js
+++ TransWord/js/main.js
@@ -1,24 +1,39 @@
 const backToTopbtn = document.querySelector('#fixed-btn');
-const inputField = document.getElementsByName("input");
+const navbar = document.querySelector("nav");
+
 
 
 backToTopbtn.style.display = 'none';
-// show scroll button 
-window.onscroll = () => {
-  if(window.pageYOffset > 300) {
+
+// show scroll button using event listner 
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 300) {
     backToTopbtn.style.display = 'block';
   } else {
     backToTopbtn.style.display = 'none';
   }
-}
+});
 
-// function scroll() {
-//   window.scrollTo(0,0);
-// }
+// scroll to the top of the page
 backToTopbtn.addEventListener('click', () => {
   window.scrollTo({
-    top:0,
-    left:0,
+    top: 0,
+    left: 0,
     behavior: 'smooth'
   });
 });
+
+
+
+
+window.onscroll = () => {
+  const showcase = document.querySelector("#showcase")
+
+  if (window.scrollY > (showcase.offsetTop + showcase.offsetHeight)) {
+    navbar.classList.add("bg-dark");
+  } else {
+    navbar.classList.remove("bg-dark");
+  }
+};
+
+   
